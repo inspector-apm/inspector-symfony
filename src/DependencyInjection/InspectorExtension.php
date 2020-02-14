07b080da9aa95c63d239000ce79a9febdfd81bbd
Valerio Barbera <ilvalerione@gmail.com>
2020-02-14 10:00:39 +0100

    inspector service injection

--- src/DependencyInjection/InspectorExtension.php
+++ src/DependencyInjection/InspectorExtension.php
@@ -6,6 +6,7 @@ namespace Inspector\Symfony\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -18,5 +19,21 @@ class InspectorExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $definition = $container->getDefinition('inspector');
+
+        // Inspector configuration
+        $inspectorConfigDefinition = new Definition(\Inspector\Configuration::class, [$config['api_key']]);
+        $inspectorConfigDefinition->setPublic(false);
+        $inspectorConfigDefinition->addMethodCall('setEnabled', [$config['enabled']]);
+        $inspectorConfigDefinition->addMethodCall('setUrl', [$config['url']]);
+        $inspectorConfigDefinition->addMethodCall('setTransport', [$config['transport']]);
+
+        $container->setDefinition('inspector.configuration', $inspectorConfigDefinition);
+
+        $definition->setArgument(0, $inspectorConfigDefinition);
     }
 }
