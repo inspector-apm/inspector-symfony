17e8b3a1f3118669cccd414f556c91125aa9b59d
Serhii Smirnov <free.smilesrg@gmail.com>
2021-05-27 20:34:45 +0300

    Updated ConsoleEventsSubscriber

--- src/Listeners/ConsoleEventsSubscriber.php
+++ src/Listeners/ConsoleEventsSubscriber.php
@@ -3,6 +3,7 @@
 namespace Inspector\Symfony\Bundle\Listeners;
 
 use Inspector\Inspector;
+use Inspector\Models\Segment;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Event\ConsoleErrorEvent;
@@ -19,27 +20,15 @@ class ConsoleEventsSubscriber implements EventSubscriberInterface
 
     /**
      * @uses onConsoleStart
-     * @uses onConsoleException
      * @uses onConsoleError
      */
     public static function getSubscribedEvents(): array
     {
         // The higher the priority number, the earlier the method is called.
-
         $listeners = [
             ConsoleEvents::COMMAND => ['onConsoleStart', 9999],
+            ConsoleEvents::ERROR => ['onConsoleError', 128],
         ];
-
-        // @todo: investigate and clean up
-        // Added ConsoleEvents in Symfony 2.3
-        if (class_exists(ConsoleEvents::class)) {
-            // Added with ConsoleEvents::ERROR in Symfony 3.3 to deprecate ConsoleEvents::EXCEPTION
-            if (class_exists(ConsoleErrorEvent::class)) {
-                $listeners[ConsoleEvents::ERROR] = ['onConsoleError', 128];
-            } else {
-                $listeners[ConsoleEvents::EXCEPTION] = ['onConsoleException', 128];
-            }
-        }
 
         return $listeners;
     }
@@ -55,20 +44,6 @@ class ConsoleEventsSubscriber implements EventSubscriberInterface
         $this->startTransaction($event->getCommand()->getName());
     }
 
-
-    /**
-     * Handle a console exception (used instead of ConsoleErrorEvent before
-     * Symfony 3.3 and kept for backwards compatibility).
-     *
-     * @throws \Exception
-     */
-    public function onConsoleException(ConsoleErrorEvent $event): void
-    {
-        $this->startTransaction($event->getCommand()->getName())->setResult('error');
-
-        $this->notifyUnexpectedError($event->getError());
-    }
-
     /**
      * Handle a console error.
      *
@@ -76,7 +51,7 @@ class ConsoleEventsSubscriber implements EventSubscriberInterface
      */
     public function onConsoleError(ConsoleErrorEvent $event): void
     {
-        $this->startTransaction($event->getCommand()->getName())->setResult('error');
+        $this->inspector->currentTransaction()->setResult('error');
 
         $this->notifyUnexpectedError($event->getError());
     }
