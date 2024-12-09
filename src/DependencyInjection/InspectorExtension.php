0d55c743f94fd4018c77e2e77528c99d3e238e50
Valerio Barbera <ilvalerione@gmail.com>
2024-12-09 11:58:04 +0100

    fix sql segment creation when disabled

--- src/DependencyInjection/InspectorExtension.php
+++ src/DependencyInjection/InspectorExtension.php
@@ -22,7 +22,7 @@ class InspectorExtension extends Extension
     /**
      * Current version of the bundle.
      */
-    const VERSION = '1.3.2';
+    const VERSION = '1.3.3';
 
     /**
      * Loads a specific configuration.
