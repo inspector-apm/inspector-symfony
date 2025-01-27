12f9d171cb2e2a2d650faf91d16c5bd35c988145
Valerio Barbera <ilvalerione@gmail.com>
2025-01-27 15:13:29 +0100

    update version

--- src/DependencyInjection/InspectorExtension.php
+++ src/DependencyInjection/InspectorExtension.php
@@ -24,7 +24,7 @@ class InspectorExtension extends Extension implements PrependExtensionInterface
     /**
      * Current version of the bundle.
      */
-    const VERSION = '1.7.0';
+    const VERSION = '1.7.1';
 
     public function prepend(ContainerBuilder $container): void
     {
